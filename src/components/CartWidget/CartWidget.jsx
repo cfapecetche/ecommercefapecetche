211149d9a91055123211606cba66c57548cde502
unknown <cfapecetche@gmail.com>
2023-10-04 16:26:05 -0300

    1er Entrega

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,17 @@
+import cart from './assets/cart.svg'
+
+
+const CartWidget = () => {
+      return(
+       <div>
+       <img scr={cart} alt="cart-widget"/>
+        0
+       </div>
+
+
+      )
+
+
+}
+
+export default CartWidget
